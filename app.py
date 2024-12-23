e44433589bc389b05597ad8a649a477679f3ba31
rohy <rohollahnaghipour41@gmail.com>
2024-12-23 23:49:30 +0330

    first commit

---
+++ app.py
@@ -0,0 +1,92 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+# تنظیمات Mediapipe
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+def blur_skin(image):
+    with mp_holistic.Holistic(static_image_mode=True) as holistic:
+        # تبدیل تصویر به RGB برای Mediapipe
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image_rgb)
+        # اگر بدنی شناسایی نشد
+        if not results.pose_landmarks:
+            print("هیچ انسانی شناسایی نشد!")
+            return image
+        else:
+            print("okkkkkkk")
+        # تشخیص نواحی بدن
+        mask = np.zeros(image.shape[:2], dtype=np.uint8)
+        for landmark in results.pose_landmarks.landmark:
+            x = int(landmark.x * image.shape[1])
+            y = int(landmark.y * image.shape[0])
+            cv2.circle(mask, (x, y), 5, 255, -1)
+        # ایجاد ماسک برای پوست
+        skin_mask = cv2.inRange(image, (0, 30, 60), (20, 150, 255))
+        # ترکیب ماسک Mediapipe با ماسک پوست
+        combined_mask = cv2.bitwise_and(mask, skin_mask)
+        # اعمال بلور روی مناطق پوست
+        blurred_image = image.copy()
+        blurred_image[combined_mask > 0] = cv2.GaussianBlur(
+            image, (15, 15), 0)[combined_mask > 0]
+        return blurred_image
+    
+
+with open('input.jpg', 'rb') as f:
+    image = cv2.imdecode(np.frombuffer(f.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+output_image = blur_skin(image)
+
+cv2.imwrite('output.jpg', output_image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+print("enddddddddddddddddddddddddd")
+
+#if not results.pose_landmarks:
+    #print("!هیچ انسانی شناسایی نشد")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#image = cv2.imread("input.jpg")
+
+#cv2.imwrite("output.jpg", output_image)
+#print("تصویر پردازش شد و ذخیره گردید!")
+
+
+
---
+++ test2.py
@@ -0,0 +1,40 @@
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+def f(image):
+    with open('input.jpg', 'rb') as f:
+        image = cv2.imdecode(np.frombuffer(f.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    img = cv2.imread('image')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def face_detection(image):
+    with mp_holistic.Holistic(static_image_mode=True) as holistic:
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = holistic.process(image_rgb)
+
+    mask = np.zeros(image.shape[:2], dtype=np.uint8)
+
+    
+    return cv2.imwrite('output.jpg', [int(cv2.IMWRITE_JPEG_QUALITY), 95])    
+
+
+with open('input.jpg', 'rb') as f:
+    image = cv2.imdecode(np.frombuffer(f.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+
+face_detection(image)
+
+
